3ba39e9a38de58fefb094d869bee0a0ccf232b56
lSoleyl <lSoleyl+github@gmail.com>
2016-11-19 00:02:20 +0100

    Loading template from separate file, changed overview into a directive.

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,16 @@
 var bankjs = angular.module('bankjs', [])
 
-bankjs.controller('overview', ['$scope', '$interval', function($scope, $interval) {
-  $scope.time = (new Date()).toLocaleTimeString()
-  $interval(function() {
-    $scope.time = (new Date()).toLocaleTimeString()
-  }, 1000)
+bankjs.directive('overview', ['$interval', function($interval) {
+  
+  return {
+    templateUrl:'templates/overview.html',
+    restrict:'E',
+    link:function($scope) {
+      $scope.time = (new Date()).toLocaleTimeString()
+      $interval(function() {
+        $scope.time = (new Date()).toLocaleTimeString()
+      }, 1000)
+    }
+  }
 }])
+
