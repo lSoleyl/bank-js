99508ee3f8bb02d611bfaec61ab57df295c8cf43
lSoleyl <lSoleyl+github@gmail.com>
2016-11-30 18:31:41 +0100

    Defined storage as ES6 class.

--- scripts/services/storage.js
+++ scripts/services/storage.js
@@ -2,32 +2,48 @@
  *  It implements a simple key-value storage like redis
  */ 
 bankjs.factory('storage', ['config', function(config) {
-  var data = {}
+  
+  //The class representing the returned storage service
+  class Storage {
+    //Construct and load storage object
+    constructor(storageKey) {
+      this.storage_key = storageKey
+      this.data = {}
+      this.size = 0
+      this.load()
+    }
 
-  //The service object to return
-  var service = {}
+    //Retrieve a stored value
+    get(key) {
+      return this.data[key]
+    }
 
-  // Retrieve a stored value
-  service.get = function(key) {
-    return data[key]
+    //Store a value
+    put(key, value) {
+      this.data[key] = value
+    }
+
+    //Save the data
+    save() {
+      const rawData = JSON.stringify(this.data)
+      localStorage.setItem(this.storage_key, rawData)
+      this.size = rawData.length
+    }
+
+    //Load the data from the local storage
+    load() {
+      const rawData = localStorage.getItem(this.storage_key)
+      this.size = rawData ? rawData.length : 0
+      this.data = JSON.parse(rawData || "{}")
+    }
+
+    //Clear the whole storage
+    clear() {
+      this.data = {}
+      this.size = 0
+      localStorage.removeItem(this.storage_key)
+    }
   }
-  //Store a value
-  service.put = function(key, value) {
-    data[key] = value
-  }
 
-  //Save the currently set data into the local storage
-  service.save = function() {
-    localStorage.setItem(config.backend.storage_key, JSON.stringify(data))
-  }
-
-  //Load the data from the local storage
-  service.load = function() {
-    data = JSON.parse(localStorage.getItem(config.backend.storage_key) || "{}")
-  }
-
-  //Load data upon service creation
-  service.load()
-
-  return service
+  return new Storage(config.backend.storage_key)
 }])
